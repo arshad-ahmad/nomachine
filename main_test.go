a885e8399243b6c3305064a59272403bce4057e0
Arshad Ahmad <arshad.hlife@gmail.com>
2021-11-13 09:08:12 +0000

    first commit

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "regexp"
+    "testing"
+)
+
+const verRegex string = `^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?` +
+    `(-([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
+    `(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?$`
+
+func TestVersion(t *testing.T) {
+    if ok, _ := regexp.MatchString(verRegex, VERSION); !ok {
+        t.Fatalf("invalid version: %s", VERSION)
+    }
+}
